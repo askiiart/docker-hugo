9f2360c6ac8e9d79feda444f471480c0130c4b1f
askiiart <dev@askiiart.net>
2023-08-28 17:56:21 -0500

    Replace wget with curl

--- download-build-push.sh
+++ download-build-push.sh
@@ -3,8 +3,8 @@ ORG=askiiart
 NAME=hugo
 PUSH=yes
 
-curl -s https://api.github.com/repos/sass/dart-sass/releases/latest | grep "browser_download_url.*dart-sass-.*-linux-x64.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*hugo_.*_linux-amd64.tar.gz" | head --lines 1 | cut -d : -f 2,3 | tr -d \" | wget -qi -
+curl $(curl -s https://api.github.com/repos/sass/dart-sass/releases/latest | grep "browser_download_url.*dart-sass-.*-linux-x64.tar.gz" | cut -d : -f 2,3 | tr -d \") -LO
+curl $(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*hugo_.*_linux-amd64.tar.gz" | head --lines 1 | cut -d : -f 2,3 | tr -d \") -LO
 
 tar -xzf dart-sass-*.tar.gz
 tar -xzf hugo_*_linux-amd64.tar.gz --one-top-level
