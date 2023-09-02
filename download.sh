7c38128019f6c56a5c66665a615b4e457162a0c8
askiiart <dev@askiiart.net>
2023-09-01 22:00:46 -0500

    Change to download-only

--- download.sh
+++ download.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env sh
-ORG=askiiart
-NAME=hugo
-PUSH=yes
-
 curl $(curl -s https://api.github.com/repos/sass/dart-sass/releases/latest | grep "browser_download_url.*dart-sass-.*-linux-x64.tar.gz" | cut -d : -f 2,3 | tr -d \") -LO
 curl $(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*hugo_.*_linux-amd64.tar.gz" | head --lines 1 | cut -d : -f 2,3 | tr -d \") -LO
-
 tar -xzf dart-sass-*.tar.gz
 tar -xzf hugo_*_linux-amd64.tar.gz --one-top-level
-
-VERSION=$(find . -name "hugo_*_linux-amd64" | sed 's/.*_\(.*\)_.*/\1/')
-ID=$(docker build . -q)
-
-if PUSH=yes; then
-    echo hi
-    exit
-fi
-
-docker tag ${ID} ${ORG}/${NAME}:latest
-docker tag ${ID} ${ORG}/${NAME}:v${VERSION}
-docker push ${ORG}/${NAME}:latest
-docker push ${ORG}/${NAME}:v${VERSION}
-
